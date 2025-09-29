ce6e21dadc820af1acdc112b37560f025155d4fd
srinivas280205 <srinivas280205@gmail.com>
2025-09-29 11:48:09 +0530

    Time: 1 ms (79.68%) | Memory: 41.6 MB (49.36%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -1,25 +1,23 @@
+import java.util.*;
+
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         String[] words = s.split(" ");
         if (pattern.length() != words.length) return false;
 
+        Map<Character, String> map = new HashMap<>();
+        Set<String> usedWords = new HashSet<>();
+
         for (int i = 0; i < pattern.length(); i++) {
-            char c1 = pattern.charAt(i);
-            String w1 = words[i];
+            char c = pattern.charAt(i);
+            String word = words[i];
 
-            // Check previous mappings
-            for (int j = 0; j < i; j++) {
-                char c2 = pattern.charAt(j);
-                String w2 = words[j];
-
-                // If letters are the same, words must match
-                if (c1 == c2 && !w1.equals(w2)) {
-                    return false;
-                }
-                // If words are the same, letters must match
-                if (w1.equals(w2) && c1 != c2) {
-                    return false;
-                }
+            if (map.containsKey(c)) {
+                if (!map.get(c).equals(word)) return false; // mismatch
+            } else {
+                if (usedWords.contains(word)) return false; // word already mapped
+                map.put(c, word);
+                usedWords.add(word);
             }
         }
 
