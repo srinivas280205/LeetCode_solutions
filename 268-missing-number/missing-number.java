4e635e3fb386a5b1720dca3f83109c7427fbec62
srinivas280205 <srinivas280205@gmail.com>
2025-09-29 11:41:30 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (72.57%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,25 +1,20 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-
-        // Check each number from 0 to n
+        boolean[] seen = new boolean[n + 1]; // to mark numbers from 0 to n
+        
+        // Mark all numbers that appear in the array
+        for (int num : nums) {
+            seen[num] = true;
+        }
+        
+        // The number that is not seen is the missing number
         for (int i = 0; i <= n; i++) {
-            boolean found = false;
-            
-            // Look for i in the array
-            for (int num : nums) {
-                if (num == i) {
-                    found = true;
-                    break;
-                }
-            }
-            
-            // If i is not found, it's the missing number
-            if (!found) {
+            if (!seen[i]) {
                 return i;
             }
         }
         
-        return -1; // This will never happen according to the problem constraints
+        return -1; // This won't happen according to the problem constraints
     }
 }
