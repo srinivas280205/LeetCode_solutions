5d5732b1eeee34139497ee25eeee17748c38997e
srinivas280205 <srinivas280205@gmail.com>
2025-10-04 10:37:41 +0530

    Time: 0 ms (100.00%) | Memory: 48.8 MB (74.17%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+
+        while (left < right) {
+            // swap characters
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            // move pointers
+            left++;
+            right--;
+        }
+    }
+}
+
