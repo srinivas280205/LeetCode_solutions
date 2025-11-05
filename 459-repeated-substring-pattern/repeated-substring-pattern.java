38fc62db8d4bd29e71654e87436084915512f0e8
srinivas280205 <srinivas280205@gmail.com>
2025-11-05 18:55:31 +0530

    Time: 6 ms (98.88%) | Memory: 46.9 MB (5.23%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.java
+++ 459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -1,8 +1,20 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-        // Double the string and remove the first and last character
-        String doubled = s + s;
-        // If s exists inside doubled (excluding first and last char), it's a repeat
-        return doubled.substring(1, doubled.length() - 1).contains(s);
+        int n = s.length();
+        int[] lps = new int[n]; // Longest Prefix Suffix array
+        int len = 0, i = 1;
+
+        while (i < n) {
+            if (s.charAt(i) == s.charAt(len)) {
+                lps[i++] = ++len;
+            } else if (len > 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i++] = 0;
+            }
+        }
+
+        int lastLPS = lps[n - 1];
+        return lastLPS > 0 && n % (n - lastLPS) == 0;
     }
 }
