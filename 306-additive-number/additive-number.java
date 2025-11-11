927905df29cdc6cec8ac08daf4b4c4ab64f817f2
srinivas280205 <srinivas280205@gmail.com>
2025-11-11 22:29:41 +0530

    Time: 6 ms (10.86%) | Memory: 43.8 MB (5.06%) - LeetSync

---
+++ 306-additive-number/additive-number.java
@@ -0,0 +1,43 @@
+import java.math.BigInteger;
+
+class Solution {
+    public boolean isAdditiveNumber(String num) {
+        int n = num.length();
+
+        for (int i = 1; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                String a = num.substring(0, i);
+                String b = num.substring(i, j);
+
+                // Leading zero check
+                if ((a.length() > 1 && a.charAt(0) == '0') ||
+                    (b.length() > 1 && b.charAt(0) == '0')) {
+                    continue;
+                }
+
+                BigInteger x = new BigInteger(a);
+                BigInteger y = new BigInteger(b);
+
+                int k = j;
+
+                while (k < n) {
+                    BigInteger z = x.add(y);      // BigInteger sum
+                    String zStr = z.toString();   // Compare as string
+
+                    if (!num.startsWith(zStr, k)) {
+                        break;
+                    }
+
+                    k += zStr.length();
+                    x = y;
+                    y = z;
+                }
+
+                if (k == n) return true;
+            }
+        }
+
+        return false;
+    }
+}
